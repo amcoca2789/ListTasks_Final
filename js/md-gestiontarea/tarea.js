a4aa7900804ca64920f0d35b95635597d8f27bf5
Ale <amcoca2789@hotmail.com>
2014-09-04 00:44:47 -0400

    Modulo- Gestión Tarea - Correcciones
    Corrección con el despliegue de los formularios con JQuery, obtención de
    los IDs, etc

--- js/md-gestiontarea/tarea.js
+++ js/md-gestiontarea/tarea.js
@@ -1,22 +1,34 @@
 var name_btn_edicion = "btn-edicion";
+var name_btn_new_tarea = "btn_new_tarea_";
 var name_form_edicion = "form-edicion";
-var name_form_newtarea = "form-newtarea";
+var name_form_newtarea = "form-newtarea-";
 
 $(document).ready(function() {
-	$("#btn_new_tarea_dia1").on('click', mostrarFormularioNewTarea);
-    $("#btn-edicion-dia1-tarea1").on('click', mostrarFormularioEdicionTarea);
+	$(".u-boton-newtarea").on('click', mostrarFormularioNewTarea);
+    $(".u-boton-ediciontarea").on('click', mostrarFormularioEdicionTarea);
 });
 
-function mostrarFormularioNewTarea () {
-	$("#form-newtarea-dia1").slideToggle(300);
+function mostrarFormularioNewTarea (evnt) {
+	evnt.preventDefault();
+
+	var id = $(this).attr('id');
+	console.log("id:"+id);
+
+	var idFormulario = id.replace(name_btn_new_tarea, name_form_newtarea);
+
+	console.log("idFormulario:"+idFormulario);
+	$("#"+idFormulario).slideToggle(100);
 }
 
 function mostrarFormularioEdicionTarea (evnt) {
 	evnt.preventDefault();
 
 	var id = $(this).attr('id');
+	console.log("id:"+id);
 
 	var idFormulario = id.replace(name_btn_edicion, name_form_edicion);
 
+
+	console.log("idFormulario:"+idFormulario);
 	$("#"+idFormulario).slideToggle(100);
 }
